79268a4fa14d44073d29a04ecf0002fe48d9c693
JelleJ18 <jellejankowski8@gmail.com>
2023-12-15 14:52:01 +0100

    .

--- App.java
+++ App.java
@@ -4,12 +4,9 @@ import java.sql.SQLException;
 
 public class App {
   public static void main(String[] args) {
-    String dburl = "jdbc:sqlserver://aei-sql2.avans.nl,1443; databaseName=DatabaseNaam;";
-    String user = "usernaam";
-    String pass = "wachtwoord";
-
+    String dburl = "jdbc:sqlserver://localhost;databaseName=DatabaseNaam;user=usernaam;password=wachtwoord;encrypt=true;TrustServerCertificate=True";
     try {
-      Connection connection = DriverManager.getConnection(dburl, user, pass);
+      Connection connection = DriverManager.getConnection(dburl);
       System.out.println("Connected!");
     } catch (SQLException e) {
       System.out.println("Oops. error!");
