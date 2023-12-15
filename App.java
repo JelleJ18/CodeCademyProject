3332e282e5c5d8ad0a2718d494841e98584e0496
JelleJ18 <jellejankowski8@gmail.com>
2023-12-15 15:55:28 +0100

    cool

--- App.java
+++ App.java
@@ -1,14 +1,15 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class App {
   public static void main(String[] args) {
-    String dburl = "jdbc:sqlserver://localhost;databaseName=DatabaseNaam;user=usernaam;password=wachtwoord;encrypt=true;TrustServerCertificate=True";
+    String url = "jdbc:sqlserver://localhost;databaseName=DatabaseNaam;user=usernaam;password=wachtwoord;encrypt=true;trustServerCertificate=true";
+    String username = "usernaam";
+    String password = "wachtwoord";
     try {
-      Connection connection = DriverManager.getConnection(dburl);
+      Connection connection = DriverManager.getConnection(url);
       System.out.println("Connected!");
-    } catch (SQLException e) {
+
+    } catch (Exception e) {
       System.out.println("Oops. error!");
       e.printStackTrace();
     }
