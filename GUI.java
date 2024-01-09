05d84db6e179136bc669f4038839246ebf29a6d4
JelleJ18 <jellejankowski8@gmail.com>
2024-01-09 20:41:05 +0100

    Added new GUI that shows an icon and added icon

--- GUI.java
+++ GUI.java
@@ -1,14 +1,27 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Hello World!"); // Komt bovenin
-        Label label = new Label("Hello world!"); // Dit is een node
-        Scene mainView = new Scene(label);
+
+        Group root = new Group();
+        Scene mainView = new Scene(root, Color.BLACK);
+
+        Image icon = new Image("icon.jpg");
+        stage.getIcons().add(icon);
+        stage.setTitle("CodeCademy Application");
+        stage.setWidth(420);
+        stage.setHeight(420);
+        stage.setResizable(false);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("Click escape to escape!");
+
         stage.setScene(mainView);
         stage.show();
     }
